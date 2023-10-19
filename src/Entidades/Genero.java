079c718ff01291b32db3acc6cbc004aee9a74374
mandiqs <136293700+mandiqs@users.noreply.github.com>
2023-10-19 14:44:57 -0300

    Add files via upload

---
+++ src/Entidades/Genero.java
@@ -0,0 +1,29 @@
+package Entidades;
+
+public class Genero {
+    protected int idGenero;
+    protected String tipoGenero;
+
+
+    public Genero(int idGenero, String tipoGenero) {
+        this.idGenero = idGenero;
+        this.tipoGenero = tipoGenero;
+    }
+
+
+    public int getIdGenero() {
+        return idGenero;
+    }
+
+    public void setIdGenero(int idGenero) {
+        this.idGenero = idGenero;
+    }
+
+    public String getTipoGenero() {
+        return tipoGenero;
+    }
+
+    public void setTipoGenero(String tipoGenero) {
+        this.tipoGenero = tipoGenero;
+    }
+}
