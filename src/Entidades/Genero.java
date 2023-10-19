a093f638a1881bf8bb82780a2138be5f67a401e4
carloshobmeier <carloshobmeier@hotmail.com>
2023-10-19 15:51:35 -0300

    merge resolution 2

--- src/Entidades/Genero.java
+++ src/Entidades/Genero.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package Entidades;
 
 public class Genero {
@@ -28,34 +28,3 @@ public class Genero {
         this.tipoGenero = tipoGenero;
     }
 }
-=======
-package Entidades;
-
-public class Genero {
-    protected int idGenero;
-    protected String tipoGenero;
-
-
-    public Genero(int idGenero, String tipoGenero) {
-        this.idGenero = idGenero;
-        this.tipoGenero = tipoGenero;
-    }
-
-
-    public int getIdGenero() {
-        return idGenero;
-    }
-
-    public void setIdGenero(int idGenero) {
-        this.idGenero = idGenero;
-    }
-
-    public String getTipoGenero() {
-        return tipoGenero;
-    }
-
-    public void setTipoGenero(String tipoGenero) {
-        this.tipoGenero = tipoGenero;
-    }
-}
->>>>>>> 079c718ff01291b32db3acc6cbc004aee9a74374
